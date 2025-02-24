b8a41fea5481d0d78c1574117152a824e13ec742
Temurbek Mansurov <Diluc-Kirara>
2025-02-24 17:07:31 +0500

    name sadsa

--- TEsts/Program.cs
+++ TEsts/Program.cs
@@ -1,9 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.Write("Hohlagan soningizni kriting: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Sizning soningiz: {number}");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+ 
